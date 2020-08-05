ea10986533effcd4332c058c17909844f1628613
Melis Cagan <meliscagan@gmail.com>
2020-08-05 13:51:49 -0400

    create homepage

---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Homepage() {
+  return (
+    <div class="landing">
+      <div class="home-wrap">
+        <div class="home-inner">
+          <h1>LARADUYMUS</h1>
+          <div class="nav">
+            <button class="navbutton">WHO</button>
+            <button class="navbutton">WHAT</button>
+            <button class="navbutton">HOW</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
