fae2af949a9afd34cce62ae831db091ccc7e0c70
Melis Cagan <meliscagan@gmail.com>
2020-08-05 13:50:47 -0400

    Updates

---
+++ precache-manifest.87aee54f7d3da90e469a32e6c576599c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854c25aa8a9aba3b9ced725641a81297",
+    "url": "/mcagan.github.io/laraduymus/index.html"
+  },
+  {
+    "revision": "2fce864d04e49274d1cd",
+    "url": "/mcagan.github.io/laraduymus/static/css/main.8b5f1ace.chunk.css"
+  },
+  {
+    "revision": "0a7313620a58cbdef45e",
+    "url": "/mcagan.github.io/laraduymus/static/js/2.87a5eb5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mcagan.github.io/laraduymus/static/js/2.87a5eb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fce864d04e49274d1cd",
+    "url": "/mcagan.github.io/laraduymus/static/js/main.50859e64.chunk.js"
+  },
+  {
+    "revision": "a813816145986b70854d",
+    "url": "/mcagan.github.io/laraduymus/static/js/runtime-main.4c98df33.js"
+  },
+  {
+    "revision": "2b45aa04daf0d55ab05d743138b6fc0c",
+    "url": "/mcagan.github.io/laraduymus/static/media/homepage.2b45aa04.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mcagan.github.io/laraduymus/static/media/logo.5d5d9eef.svg"
+  }
+]);
